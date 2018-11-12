dd2aaf9103380af3213d33bbe829632f74592c33
Trolle <trolley@kth.se>
2018-11-12 15:20:18 +0000

    Added a collapsible feed

--- react-native/screens/FeedScreen.js
+++ react-native/screens/FeedScreen.js
@@ -1,60 +1,111 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  View,
+  Switch,
+  ScrollView,
   StyleSheet,
   Text,
-  Platform,
+  View,
   TouchableOpacity,
 } from 'react-native';
-import Timeline from 'react-native-timeline-listview';
-import { Icon } from 'expo';
-import Colors from '../constants/Colors';
+import { Constants } from 'expo';
+import Collapsible from 'react-native-collapsible';
+import Accordion from 'react-native-collapsible/Accordion';
 
-const dummyData = [
+const BACON_IPSUM =
+  'Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs. Picanha beef prosciutto meatball turkey shoulder shank salami cupim doner jowl pork belly cow. Chicken shankle rump swine tail frankfurter meatloaf ground round flank ham hock tongue shank andouille boudin brisket. ';
+
+const CONTENT = [
   {
-    time: '09:00',
-    title: 'Event 1',
-    description: 'Event 1 Description',
-    type: 'chat',
+    title: BACON_IPSUM,
+    content: BACON_IPSUM,
   },
-  { time: '10:45', title: 'Event 2', description: 'Event 2 Description' },
-  { time: '12:00', title: 'Event 3', description: 'Event 3 Description' },
-  { time: '14:00', title: 'Event 4', description: 'Event 4 Description' },
-  { time: '16:30', title: 'Event 5', description: 'Event 5 Description' },
-  { time: '10:45', title: 'Event 2', description: 'Event 2 Description' },
-  { time: '12:00', title: 'Event 3', description: 'Event 3 Description' },
-  { time: '14:00', title: 'Event 4', description: 'Event 4 Description' },
-  { time: '16:30', title: 'Event 5', description: 'Event 5 Description' },
-  { time: '10:45', title: 'Event 2', description: 'Event 2 Description' },
-  { time: '12:00', title: 'Event 3', description: 'Event 3 Description' },
-  { time: '14:00', title: 'Event 4', description: 'Event 4 Description' },
-  { time: '16:30', title: 'Event 5', description: 'Event 5 Description' },
+  {
+    title: 'Second',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Third',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Fourth',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Fifth',
+    content: BACON_IPSUM,
+  },
 ];
-//OnEventPress: e = {
-//   description: 'Event 4 Description',
-//   time: '14:00',
-//   title: 'Event 4',
-// };
-export default class FeedScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Feed',
+
+const SELECTORS = [
+  {
+    title: 'First',
+    value: 0,
+  },
+  {
+    title: 'Third',
+    value: 2,
+  },
+  {
+    title: 'None',
+  },
+];
+
+export default class FeedScreen extends Component {
+  state = {
+    activeSections: [],
+    collapsed: true,
+    multipleSelect: false,
   };
 
+  toggleExpanded = () => {
+    this.setState({ collapsed: !this.state.collapsed });
+  };
+
+  setSections = sections => {
+    this.setState({
+      activeSections: sections.includes(undefined) ? [] : sections,
+    });
+  };
+
+  renderHeader = (section, _, isActive) => {
+    return (
+      <View style={[styles.header, isActive ? styles.active : styles.inactive]}>
+        <View style={styles.dot} />
+        <View style={styles.headerLine} />
+        <Text style={styles.headerText}>{section.title}</Text>
+      </View>
+    );
+  };
+
+  renderContent(section, _, isActive) {
+    return (
+      <View
+        style={[styles.content, isActive ? styles.active : styles.inactive]}
+      >
+        <View style={styles.line} />
+        <Text>{section.content}</Text>
+      </View>
+    );
+  }
+
   render() {
-    const { navigate } = this.props.navigation;
+    const { multipleSelect, activeSections } = this.state;
+
     return (
       <View style={styles.container}>
-        <Timeline data={dummyData} onEventPress={e => console.log(e)} />
-        <TouchableOpacity
-          style={styles.addButton}
-          onPress={() => navigate('Select')}
-        >
-          <Icon.Ionicons
-            name={Platform.OS === 'ios' ? `ios-add-circle` : 'md-add-circle'}
-            size={50}
-            color={Colors.tabIconDefault}
+        <ScrollView contentContainerStyle={{ paddingTop: 30 }}>
+          <Accordion
+            activeSections={activeSections}
+            sections={CONTENT}
+            touchableComponent={TouchableOpacity}
+            expandMultiple={multipleSelect}
+            renderHeader={this.renderHeader}
+            renderContent={this.renderContent}
+            duration={400}
+            onChange={this.setSections}
           />
-        </TouchableOpacity>
+        </ScrollView>
       </View>
     );
   }
@@ -63,11 +114,83 @@ export default class FeedScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F5FCFF',
+    paddingTop: Constants.statusBarHeight,
   },
-  addButton: {
+  title: {
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: '300',
+    marginBottom: 20,
+  },
+  header: {
+    backgroundColor: '#F5FCFF',
+    paddingLeft: 10,
+    paddingRight: 10,
+    flexDirection: 'row',
+    position: 'relative',
+  },
+  headerText: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  content: {
+    padding: 21.5,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+  },
+  active: {
+    backgroundColor: 'rgba(255,255,255,1)',
+  },
+  inactive: {
+    backgroundColor: 'rgba(245,252,255,1)',
+  },
+  selectors: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  selector: {
+    backgroundColor: '#F5FCFF',
+    padding: 10,
+  },
+  activeSelector: {
+    fontWeight: 'bold',
+  },
+  selectTitle: {
+    fontSize: 14,
+    fontWeight: '500',
+    padding: 10,
+  },
+  multipleToggle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 30,
+    alignItems: 'center',
+  },
+  multipleToggle__title: {
+    fontSize: 16,
+    marginRight: 8,
+  },
+  dot: {
+    height: 25,
+    width: 25,
+    backgroundColor: '#bbb',
+    borderRadius: 50,
+  },
+  line: {
+    width: 2,
+    backgroundColor: '#bbb',
+    marginTop: -100,
+    marginBottom: -100,
+    marginRight: 10,
+  },
+  headerLine: {
+    width: 2,
+    height: '100%',
+    backgroundColor: '#bbb',
     position: 'absolute',
-    bottom: 10,
-    right: 10,
+    left: 9 + 25 / 2,
   },
 });
