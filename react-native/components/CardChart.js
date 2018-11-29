255b6e8c43d29a29a68a9a4e7b3434576ee580bb
arvidlarzzon <arvidlarzzon@hotmail.com>
2018-11-29 22:00:04 +0100

    expo not working

--- react-native/components/CardChart.js
+++ react-native/components/CardChart.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import { Dimensions } from 'react-native';
 import { LineChart } from 'react-native-chart-kit';
+import { db } from '../config';
 
 export default class CardChart extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      data: null,
+    };
+  }
+  componentDidMount() {
+    this.getData();
   }
   getData = () => {
     // Hämta data, formatData() return
+    let res = db.getData(1, 'weight', this.onceHandler, this.formatData);
+    console.log('getData: ', res);
+    this.formatData(res);
     return null;
   };
-  formatData = () => {
-    let formattedData;
+  formatData = res => {
+    let formattedData = res;
 
     if (formattedData) {
+      this.setState({
+        data: formattedData,
+      });
+      console.log(formattedData);
     } else {
+      console.log('something went wrong');
     }
     return formattedData;
+  };
+  onceHandler = res => {
+    console.log(res);
   };
   render() {
     let { width, height } = this.props;
